997cba90c3dce702acf5f4ae204261b43ce31a74
Jack Walmsley <jackawalmsley@gmail.com>
2023-07-24 22:28:46 -0400

    Added two projects

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,65 +1,73 @@
-import Link from "next/link";
-import React from "react";
-import { allProjects } from "contentlayer/generated";
-import { Navigation } from "../components/nav";
-import { Card } from "../components/card";
-import { Article } from "./article";
-import { Eye } from "lucide-react";
+import Link from 'next/link';
+import React from 'react';
+import { allProjects } from 'contentlayer/generated';
+import { Navigation } from '../components/nav';
+import { Card } from '../components/card';
+import { Article } from './article';
+import { Eye } from 'lucide-react';
 
 export const revalidate = 60;
 export default async function ProjectsPage() {
-	const sorted = allProjects
-		.sort(
-			(a, b) =>
-				new Date(b.date ?? Number.POSITIVE_INFINITY).getTime() -
-				new Date(a.date ?? Number.POSITIVE_INFINITY).getTime(),
-		);
+    let duetomorrow = allProjects.filter((p) => p.slug === 'duetomorrow')[0];
+    const sorted = allProjects
+        .filter((p) => p !== duetomorrow)
+        .sort(
+            (a, b) =>
+                new Date(b.date ?? Number.POSITIVE_INFINITY).getTime() -
+                new Date(a.date ?? Number.POSITIVE_INFINITY).getTime()
+        );
 
-	return (
-		<div className="relative pb-16">
-			<Navigation />
-			<div className="px-6 pt-16 mx-auto space-y-8 max-w-7xl lg:px-8 md:space-y-16 md:pt-24 lg:pt-32">
-				<div className="max-w-2xl mx-auto lg:mx-0">
-					<h2 className="text-3xl font-bold tracking-tight text-zinc-100 sm:text-4xl">
-						Projects
-					</h2>
-					<p className="mt-4 text-zinc-400">
-						Some of the projects are from work and some are on my own time.
-					</p>
-				</div>
-				<div className="w-full h-px bg-zinc-800" />
-				<div className="hidden w-full h-px md:block bg-zinc-800" />
+    return (
+        <div className="relative pb-16">
+            <Navigation />
+            <div className="px-6 pt-16 mx-auto space-y-8 max-w-7xl lg:px-8 md:space-y-16 md:pt-24 lg:pt-32">
+                <div className="max-w-2xl mx-auto lg:mx-0">
+                    <h2 className="text-3xl font-bold tracking-tight text-zinc-100 sm:text-4xl">
+                        Projects
+                    </h2>
+                    <p className="mt-4 text-zinc-400">
+                        A collection of projects I've worked on, mostly for fun
+                        or for hackathons.
+                    </p>
+                </div>
+                <div className="w-full h-px bg-zinc-800" />
+                <div className="grid grid-cols-1 gap-4">
+                    <Card key={duetomorrow.slug}>
+                        <Article project={duetomorrow} />
+                    </Card>
+                </div>
+                <div className="hidden w-full h-px md:block bg-zinc-800" />
 
-				<div className="grid grid-cols-1 gap-4 mx-auto lg:mx-0 md:grid-cols-3">
-					<div className="grid grid-cols-1 gap-4">
-						{sorted
-							.filter((_, i) => i % 3 === 0)
-							.map((project) => (
-								<Card key={project.slug}>
-									<Article project={project}/>
-								</Card>
-							))}
-					</div>
-					<div className="grid grid-cols-1 gap-4">
-						{sorted
-							.filter((_, i) => i % 3 === 1)
-							.map((project) => (
-								<Card key={project.slug}>
-									<Article project={project}/>
-								</Card>
-							))}
-					</div>
-					<div className="grid grid-cols-1 gap-4">
-						{sorted
-							.filter((_, i) => i % 3 === 2)
-							.map((project) => (
-								<Card key={project.slug}>
-									<Article project={project}/>
-								</Card>
-							))}
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+                <div className="grid grid-cols-1 gap-4 mx-auto lg:mx-0 md:grid-cols-3">
+                    <div className="grid grid-cols-1 gap-4">
+                        {sorted
+                            .filter((_, i) => i % 3 === 0)
+                            .map((project) => (
+                                <Card key={project.slug}>
+                                    <Article project={project} />
+                                </Card>
+                            ))}
+                    </div>
+                    <div className="grid grid-cols-1 gap-4">
+                        {sorted
+                            .filter((_, i) => i % 3 === 1)
+                            .map((project) => (
+                                <Card key={project.slug}>
+                                    <Article project={project} />
+                                </Card>
+                            ))}
+                    </div>
+                    <div className="grid grid-cols-1 gap-4">
+                        {sorted
+                            .filter((_, i) => i % 3 === 2)
+                            .map((project) => (
+                                <Card key={project.slug}>
+                                    <Article project={project} />
+                                </Card>
+                            ))}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
