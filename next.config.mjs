6a0f5b8b915bff8015cfe7ee425ebd23e81464a4
Jack Walmsley <jackawalmsley@gmail.com>
2023-07-27 00:27:47 -0400

    Export mode

--- next.config.mjs
+++ next.config.mjs
@@ -3,10 +3,7 @@ import { withContentlayer } from "next-contentlayer";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
-	experimental: {
-		appDir: true,
-		mdxRs: true,
-	},
+	output: 'export',
 };
 
 export default withContentlayer(nextConfig);
